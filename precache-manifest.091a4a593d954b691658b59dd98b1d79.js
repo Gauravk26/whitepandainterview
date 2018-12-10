1a966146d9d51ae935b00be600de5141f9d12b25
Gaurav Kumar <gauravk446159@gmail.com>
2018-12-11 00:25:10 +0530

    Updates

---
+++ precache-manifest.091a4a593d954b691658b59dd98b1d79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a90c164d227972c9f0d1",
+    "url": "/whitepandainterview/static/js/runtime~main.a90c164d.js"
+  },
+  {
+    "revision": "9ef5dce9983d64f0baab",
+    "url": "/whitepandainterview/static/js/main.9ef5dce9.chunk.js"
+  },
+  {
+    "revision": "f4bb04f8a4ae323e35e0",
+    "url": "/whitepandainterview/static/js/1.f4bb04f8.chunk.js"
+  },
+  {
+    "revision": "9ef5dce9983d64f0baab",
+    "url": "/whitepandainterview/static/css/main.c3ecb815.chunk.css"
+  },
+  {
+    "revision": "3c2f1886bc485d39fd341578fb1dfe8a",
+    "url": "/whitepandainterview/index.html"
+  }
+];
